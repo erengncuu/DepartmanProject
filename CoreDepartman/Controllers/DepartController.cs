299326c705c70dea4954dee561692c4be87de56e
sezgin <erengncuu@outlook.com>
2023-06-06 09:37:01 +0300

    DepartController'a Departmanın verilerini tanımlandı

--- CoreDepartman/Controllers/DepartController.cs
+++ CoreDepartman/Controllers/DepartController.cs
@@ -1,12 +1,15 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CoreDepartman.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace CoreDepartman.Controllers
 {
     public class DepartController : Controller
     {
+        Context c = new Context();
         public IActionResult Index()
         {
-            return View();
+            var degerler = c.Departmans.ToList();
+            return View(degerler);
         }
     }
 }
