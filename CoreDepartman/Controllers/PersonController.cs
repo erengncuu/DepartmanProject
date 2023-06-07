fcea0c2000e19475f9b33274367c92fd0e5811f5
sezgin <erengncuu@outlook.com>
2023-06-07 14:39:26 +0300

    Staff bölümündeki tuşları çalışır hale getirme

--- CoreDepartman/Controllers/PersonController.cs
+++ CoreDepartman/Controllers/PersonController.cs
@@ -21,5 +21,26 @@ namespace CoreDepartman.Controllers
         {
             return View();
         }
+        public IActionResult DeletePerson(int id)
+        {
+            var per = c.Personels.Find(id);
+            c.Personels.Remove(per);
+            c.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public IActionResult EditPerson(int id)
+        {
+            var person = c.Personels.Find(id);
+            return View("EditPerson",person);
+        }
+        public IActionResult UpdatePerson(Personel p)
+        {
+            var per = c.Personels.Find(p.PerId);
+            per.FirstName = p.FirstName;
+            per.LastName = p.LastName;
+            per.City = p.City;
+            c.SaveChanges();
+            return RedirectToAction("Index");
+        }
     }
 }
